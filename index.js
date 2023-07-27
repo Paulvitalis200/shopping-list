1f6f6c63e667dedc46ed7bc4a2d51c61a408ee62
Paul Otieno <vitalispaul48@live.com>
2023-07-27 17:59:30 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const myList = [
+    {
+        id: 1,
+        description: 'Rubber',
+        category: 'Stationary',
+        amount: 200
+    },
+    {
+        id: 2,
+        description: 'Milk',
+        category: 'Groceries',
+        amount: 50
+    },
+    {
+        id: 3,
+        description: 'Sofa',
+        category: 'Furniture',
+        amount: 300000
+    }
+]
+
+
+// 1. Create a table of your shopping list containing the three items in the list with three columns. Description, Category and Amount
+// Fetch them from the table above and put them into  a html page with javascript.
+// Do not use any CSS. Ensure to only use Bootstrap
