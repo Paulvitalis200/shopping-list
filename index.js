54667df7e9f2826fc147d0136de1eb1a69e0b66a
Paul Otieno <vitalispaul48@live.com>
2023-07-27 18:09:49 +0300

    Update instructions

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ const myList = [
 ]
 
 
+
+
 // 1. Create a table of your shopping list containing the three items in the list with three columns. Description, Category and Amount
 // 2. Calculate the total amount using javascript's "reduce" method. Read up on it from google
+// Check the ReadMe file on Github of this project to see how the table should appear.
 // Fetch them from the table above and put them into  a html page with javascript.
-// Do not use any CSS. Ensure to only use Bootstrap
+// Do not use any CSS. Ensure to only use Bootstrap. https://getbootstrap.com/docs/4.1/content/tables/
